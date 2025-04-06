a4ca33740d76b4cf64b6f2d69f6bab5d25ee4c23
ShohelAlMahmudDev <shohelcse@yahoo.com>
2025-04-06 02:41:08 +0200

    render.yaml file has been modified31

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 settings = Settings()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://german-translator-frontend.onrender.com"],
     allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
     allow_credentials=True
